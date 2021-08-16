e45bfe329626aae90b730410dfec25220dc67827
Luis Boto <luisboto@hotmail.es>
2021-08-16 13:29:48 +0200

    Refactored cloning operation

--- src/tsp/geneticAlgorithm/GeneticFunctions.java
+++ src/tsp/geneticAlgorithm/GeneticFunctions.java
@@ -62,6 +62,38 @@ public class GeneticFunctions {
 			return new NumericValueVariable(rand.nextDouble() * 1000.0, rand.nextBoolean());
 		return new NumericValueVariable(rand.nextInt(1000), rand.nextBoolean());
 	}
+	
+	public static Operation cloneRepresentationRecursive(Operation present) {
+		String className = present.getClass().getSimpleName(); 
+		switch (className) {
+		case "Addition":
+			return new Addition(cloneRepresentationRecursive(present.getFirstOperator()),
+					cloneRepresentationRecursive(present.getSecondOperator()));
+		case "Subtraction":
+			return new Subtraction(cloneRepresentationRecursive(present.getFirstOperator()),
+					cloneRepresentationRecursive(present.getSecondOperator()));
+		case "Multiplication":
+			return new Multiplication(cloneRepresentationRecursive(present.getFirstOperator()),
+					cloneRepresentationRecursive(present.getSecondOperator()));
+		case "Division":
+			return new Division(cloneRepresentationRecursive(present.getFirstOperator()),
+					cloneRepresentationRecursive(present.getSecondOperator()));
+		case "Power":
+			return new Power(cloneRepresentationRecursive(present.getFirstOperator()),
+					cloneRepresentationRecursive(present.getSecondOperator()));
+		case "Sin":
+			return new Sin(cloneRepresentationRecursive(present.getFirstOperator()));
+		case "Cos":
+			return new Cos(cloneRepresentationRecursive(present.getFirstOperator()));
+		case "Tan":
+			return new Tan(cloneRepresentationRecursive(present.getFirstOperator()));
+		case "NumericValueVariable":
+			return new NumericValueVariable(((NumericValueVariable) present).getValue(),
+					((NumericValueVariable) present).isVariable());
+		default:
+			return null;
+		}
+	}
 
 	public static InterpolationFitnessFunction getFitnessFunction() {
 		return new InterpolationFitnessFunction();
--- src/tsp/geneticAlgorithm/Individual.java
+++ src/tsp/geneticAlgorithm/Individual.java
@@ -1,15 +1,6 @@
 package tsp.geneticAlgorithm;
 
-import tsp.lib.arithmetic.Addition;
-import tsp.lib.arithmetic.Cos;
-import tsp.lib.arithmetic.Division;
-import tsp.lib.arithmetic.Multiplication;
-import tsp.lib.arithmetic.NumericValueVariable;
 import tsp.lib.arithmetic.Operation;
-import tsp.lib.arithmetic.Power;
-import tsp.lib.arithmetic.Sin;
-import tsp.lib.arithmetic.Subtraction;
-import tsp.lib.arithmetic.Tan;
 
 public class Individual {
 	private Operation representation;
@@ -24,7 +15,7 @@ public class Individual {
 		// since otherwise new individuals which mutate from an original individual will
 		// modify the original individual's representation along the way, due to the
 		// nature of the Operation class.
-		return cloneRepresentationRecursive(this.representation);
+		return GeneticFunctions.cloneRepresentationRecursive(this.representation);
 	}
 
 	public double getFitness() {
@@ -38,37 +29,5 @@ public class Individual {
 	@Override
 	public String toString() {
 		return representation.toString();
-	}
-
-	private Operation cloneRepresentationRecursive(Operation present) {
-		String className = present.getClass().getSimpleName(); 
-		switch (className) {
-		case "Addition":
-			return new Addition(cloneRepresentationRecursive(present.getFirstOperator()),
-					cloneRepresentationRecursive(present.getSecondOperator()));
-		case "Subtraction":
-			return new Subtraction(cloneRepresentationRecursive(present.getFirstOperator()),
-					cloneRepresentationRecursive(present.getSecondOperator()));
-		case "Multiplication":
-			return new Multiplication(cloneRepresentationRecursive(present.getFirstOperator()),
-					cloneRepresentationRecursive(present.getSecondOperator()));
-		case "Division":
-			return new Division(cloneRepresentationRecursive(present.getFirstOperator()),
-					cloneRepresentationRecursive(present.getSecondOperator()));
-		case "Power":
-			return new Power(cloneRepresentationRecursive(present.getFirstOperator()),
-					cloneRepresentationRecursive(present.getSecondOperator()));
-		case "Sin":
-			return new Sin(cloneRepresentationRecursive(present.getFirstOperator()));
-		case "Cos":
-			return new Cos(cloneRepresentationRecursive(present.getFirstOperator()));
-		case "Tan":
-			return new Tan(cloneRepresentationRecursive(present.getFirstOperator()));
-		case "NumericValueVariable":
-			return new NumericValueVariable(((NumericValueVariable) present).getValue(),
-					((NumericValueVariable) present).isVariable());
-		default:
-			return null;
-		}
 	}
 }
