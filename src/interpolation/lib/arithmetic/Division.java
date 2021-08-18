ee63ffa1e659013111a7a39372ef432ac91090cb
Luis Boto <luisboto@hotmail.es>
2021-08-18 20:25:02 +0200

    Simplification ops on multiplication && division

--- src/interpolation/lib/arithmetic/Division.java
+++ src/interpolation/lib/arithmetic/Division.java
@@ -15,5 +15,31 @@ public class Division extends Operation {
 	public String toString() {
 		return "((" + firstOperator.toString() + ") / (" + secondOperator.toString() + "))";
 	}
+	
+	@Override
+	public Operation simplify() {
+		Operation simplifiedOperation = super.simplify(); // Super implementation handles recursivity
+		// Order is important! Check simple cases first
+		if (this.getFirstOperator() instanceof NumericValueVariable
+				&& this.getSecondOperator() instanceof NumericValueVariable
+				&& !((NumericValueVariable) this.getFirstOperator()).isVariable()
+				&& !((NumericValueVariable) this.getSecondOperator()).isVariable()) {
+			// Both operators are numeric values
+			double value1 = ((NumericValueVariable) this.getFirstOperator()).getValue();
+			double value2 = ((NumericValueVariable) this.getSecondOperator()).getValue();
+			return new NumericValueVariable(value1 / value2, false);
+		}
+		if (super.isZero(this.getFirstOperator()) || super.isZero(this.getSecondOperator())) {
+			if (super.isZero(this.getFirstOperator()))
+				return new NumericValueVariable(0, false);
+			if (super.isZero(this.getSecondOperator()))
+				return new NumericValueVariable(Double.MAX_VALUE, false);
+		}
+		if (this.getFirstOperator().toString().equals(this.getSecondOperator().toString())) {
+			// Both operators encode the same arithmetic operation (ex: x/x turns into 1)
+			return new NumericValueVariable(1, false);
+		}
+		return simplifiedOperation;
+	}
 
 }
--- src/interpolation/lib/arithmetic/Multiplication.java
+++ src/interpolation/lib/arithmetic/Multiplication.java
@@ -16,4 +16,28 @@ public class Multiplication extends Operation {
 		return "(" + firstOperator.toString() + ")*(" + secondOperator.toString() + ")";
 	}
 
+	@Override
+	public Operation simplify() {
+		Operation simplifiedOperation = super.simplify(); // Super implementation handles recursivity
+		// Order is important! Check simple cases first
+		if (this.getFirstOperator() instanceof NumericValueVariable
+				&& this.getSecondOperator() instanceof NumericValueVariable
+				&& !((NumericValueVariable) this.getFirstOperator()).isVariable()
+				&& !((NumericValueVariable) this.getSecondOperator()).isVariable()) {
+			// Both operators are numeric values
+			double value1 = ((NumericValueVariable) this.getFirstOperator()).getValue();
+			double value2 = ((NumericValueVariable) this.getSecondOperator()).getValue();
+			return new NumericValueVariable(value1 * value2, false);
+		}
+		if (super.isZero(this.getFirstOperator()) || super.isZero(this.getSecondOperator())) {
+			// Either operator being zero means 0
+			return new NumericValueVariable(0, false);
+		}
+		if (this.getFirstOperator().toString().equals(this.getSecondOperator().toString())) {
+			// Both operators encode the same arithmetic operation (ex: x*x turns into x²)
+			return new Power(this.getFirstOperator(), new NumericValueVariable(2, false));
+		}
+		return simplifiedOperation;
+	}
+
 }
