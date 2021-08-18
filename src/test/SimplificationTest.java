afeac888824492e797c9855a9e804651f6bc61d6
Luis Boto <luisboto@hotmail.es>
2021-08-18 20:57:30 +0200

    Added simplification test

---
+++ src/test/SimplificationTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import interpolation.geneticAlgorithm.GeneticFunctions;
+import interpolation.geneticAlgorithm.Individual;
+import interpolation.lib.arithmetic.Operation;
+import junit.framework.Assert;
+
+class SimplificationTest {
+
+	@Test
+	void simplificationTest() {
+		Operation initial;
+		Operation simplified;
+		double value1, value2;
+		for (int i = 0; i < 100000; i++) {
+			initial = GeneticFunctions.getRandomOperation();
+			simplified = GeneticFunctions.cloneRepresentationRecursive(initial).simplify();
+			for (int j = 0; j < 1000; j++) {
+				value1 = initial.computeValue(j);
+				value2 = simplified.computeValue(j);
+				if (Double.isNaN(value1))
+					value1 = value2;
+				Assert.assertEquals(value1, value2);
+			}
+		}
+	}
+
+}
